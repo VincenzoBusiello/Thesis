c584d83ad12621735282539c5bb0f12957158bd2
vincenzobusiello <126822842+VincenzoBusiello@users.noreply.github.com>
2026-02-07 16:38:04 +0100

    Update Recolorize_Thesis.R

--- Recolorize_Thesis.R
+++ Recolorize_Thesis.R
@@ -1,1 +1,11 @@
+library(terra) # used for spatial data life raster or vector data
+library(imageRy) # handling raster images
+library(ggplot2)# for graphs
+library(viridis) # alternative color palettes 
+library(patchwork) # for multiple grapsh on a single plot
+library(recolorize)
 
+
+
+# setting the working directory 
+setwd("C:/SEexam/bourgneuf")
